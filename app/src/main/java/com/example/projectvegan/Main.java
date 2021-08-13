802da6bd38128fd13f34bdfc06af37f6342a7d3f
smhrd <gks6514461@gmail.com>
2021-08-13 11:44:30 +0900

    음식리스트

--- app/src/main/java/com/example/projectvegan/Main.java
+++ app/src/main/java/com/example/projectvegan/Main.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toolbar;
@@ -34,7 +36,9 @@ public class Main extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private int[] imgs = {R.drawable.tree1,R.drawable.tree2,R.drawable.tree3};
 
+    private String id = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,22 +66,27 @@ public class Main extends AppCompatActivity {
         });
 
         Button btn_nav_logout = (Button)nav_header_view.findViewById(R.id.btn_nav_logout);
+        Button btn_nav_mypage = (Button)nav_header_view.findViewById(R.id.btn_nav_mypage);
+
         TextView tv_nav_subtitle = (TextView)nav_header_view.findViewById(R.id.tv_nav_subtitle);
-        String info =PreferenceManager.getString(getApplicationContext(),"info");
-        try {
-            JSONObject jsonObject = new JSONObject(info);
-            if(info == null){
-                btn_nav_logout.setVisibility(View.INVISIBLE);
-            }else{
-                btn_nav_logout.setVisibility(View.VISIBLE);
-                tv_nav_login.setText(jsonObject.getString("name"));
-                tv_nav_subtitle.setText(jsonObject.getString("id"));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+        TextView tv_poing = (TextView)nav_header_view.findViewById(R.id.tv_point);
+        ImageView img_tree = (ImageView)nav_header_view.findViewById(R.id.img_tree);
+
+        if(id == null){
+            btn_nav_logout.setVisibility(View.INVISIBLE);
+            btn_nav_mypage.setVisibility(View.INVISIBLE);
+            tv_poing.setVisibility(View.INVISIBLE);
+            img_tree.setVisibility(View.INVISIBLE);
+        }else{
+            btn_nav_logout.setVisibility(View.VISIBLE);
+            btn_nav_mypage.setVisibility(View.VISIBLE);
+            tv_poing.setVisibility(View.VISIBLE);
+            img_tree.setVisibility(View.VISIBLE);/*
+            tv_nav_login.setText(jsonObject.getString("name"));
+            tv_nav_subtitle.setText(jsonObject.getString("id"));*/
         }
 
-        Button btn_nav_mypage = (Button)nav_header_view.findViewById(R.id.btn_nav_mypage);
+
         btn_nav_mypage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
